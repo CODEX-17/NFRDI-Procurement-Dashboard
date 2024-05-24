ee7b7392825294500fc2e2be1e7ff47fab73fef3
RUMAR C. PAMPARO <112467114+CODEX-17@users.noreply.github.com>
2024-05-24 13:45:54 +0800

    updated db

--- database/nfrdi_procurement.sql
+++ database/nfrdi_procurement.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 07:32 AM
+-- Generation Time: May 24, 2024 at 07:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -87,11 +87,13 @@ CREATE TABLE `tbl_project_details` (
 --
 
 INSERT INTO `tbl_project_details` (`pr_no`, `accnt_id`, `type`, `title`, `contractor`, `contract_amount`, `date_published`, `status`) VALUES
-('1500-23BAC', 'fdju78hf', 1, 'test1-bid', 'DANGLI TOPING BUILDERS', '2', '2024-02-06', 'ongoing'),
-('1500-26BAC', 'fdju78hf', 1, 'test2-bid', 'DANGLI TOPING BUILDERS', '29761', '2024-05-08', 'ongoing'),
+('1500-23BAC', 'fdju78hf', 1, 'test1-bid', 'DANGLI TOPING BUILDERS', '23', '2024-01-31', 'ongoing'),
+('1500-26BAC', 'fdju78hf', 1, 'test2-bid', 'DANGLI TOPING BUILDERS', '29761', '2024-05-03', 'ongoing'),
 ('1600-23BAC', 'fdju78hf', 2, 'test1-alt', 'sad bds', '646168', '2024-02-27', 'ongoing'),
-('1600-26BAC', 'fdju78hf', 2, 'test2-alt', 'DFSKFK', '024339', '2024-02-27', 'ongoing'),
-('1700-23BAC', 'fdju78hf', 1, 'test', 'rumarsds', '565', '2024-05-14', 'ongoing');
+('1600-24BAC', 'fdju78hf', 1, 'SAMPLE TITLE', 'SAMPLE CONTRACTOR', '56', '2024-05-16', 'ongoing'),
+('1600-26BAC', 'fdju78hf', 2, 'test2-alt', 'RUMAR PAMPARO', '24339', '2024-02-23', 'ongoing'),
+('1700-23BAC', 'fdju78hf', 1, 'test', 'rumarsds', '565', '2024-05-14', 'ongoing'),
+('dasds', 'fdju78hf', 1, 'dasdsa', 'dasds', '22', '2024-05-16', 'ongoing');
 
 -- --------------------------------------------------------
 
@@ -115,11 +117,15 @@ CREATE TABLE `tbl_project_files` (
 --
 
 INSERT INTO `tbl_project_files` (`file_id`, `pr_no`, `bac_resolution`, `notice_of_award`, `contract`, `notice_to_proceed`, `philgeps_award_notice`, `date`) VALUES
-('2CExOwe3', '1500-23BAC', '1500-23BAC_bac_resolution.pdf', '1500-23BAC_notice_of_award.pdf', '1500-23BAC_contract.pdf', '1500-23BAC_notice_to_proceed.pdf', '1500-23BAC_philgeps_award_notice.pdf', '2024-02-22'),
-('6wxZniwD', '1500-26BAC', '1500-26BAC_bac_resolution.pdf', NULL, NULL, NULL, NULL, '2024-05-08'),
+('0MIFozjv', '1500-23BAC', '1500-23BAC_bac_resolution.pdf', NULL, NULL, NULL, NULL, '2024-05-13'),
+('1adai4wa', '1500-26BAC', '1500-26BAC_bac_resolution.pdf', NULL, NULL, NULL, NULL, '2024-05-14'),
 ('BHUF7exf', '1600-23BAC', '1600-23BAC_bac_resolution.pdf', '1600-23BAC_notice_of_award.pdf', '1600-23BAC_contract.pdf', '1600-23BAC_notice_to_proceed.pdf', '1600-23BAC_philgeps_award_notice.pdf', '2024-02-23'),
 ('BntN9EqC', '1700-23BAC', '1700-23BAC_bac_resolution.pdf', '1700-23BAC_notice_of_award.pdf', NULL, NULL, NULL, '2024-05-10'),
-('dmbDXQHZ', '1600-26BAC', '1600-26BAC_bac_resolution.pdf', NULL, NULL, '1600-26BAC_notice_to_proceed.pdf', NULL, '2024-03-08');
+('bsWc1OTw', '1600-24BAC', '1600-24BAC_bac_resolution.pdf', NULL, NULL, NULL, NULL, '2024-05-15'),
+('cR25NJIi', 'dasd', 'dasd_bac_resolution.pdf', 'dasd_notice_of_award.pdf', NULL, NULL, NULL, '2024-05-15'),
+('dmbDXQHZ', '1600-26BAC', '1600-26BAC_bac_resolution.pdf', '1600-26BAC_notice_of_award.pdf', NULL, NULL, NULL, '2024-03-08'),
+('paQYKwmX', 'test', 'test_bac_resolution.pdf', NULL, NULL, NULL, NULL, '2024-05-15'),
+('WPQDW0zk', 'dasds', 'dasds_bac_resolution.pdf', 'dasds_notice_of_award.pdf', NULL, NULL, NULL, '2024-05-15');
 
 --
 -- Indexes for dumped tables
